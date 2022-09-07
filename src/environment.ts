7781a863329065f43cc2bf8fd22d3f12bbda35f6
Sid Sun <sid@sidsun.com>
2022-09-07 15:53:59 +0000

    Change domain to bin.dronk.dev
    Signed-off-by: Sid Sun <sid@sidsun.com>

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    APIBaseURL: "https://api.ioctl.ml/",
-    S3BaseURL: "https://cdn.ioctl.ml/",
+    APIBaseURL: "https://api.bin.dronk.dev/",
+    S3BaseURL: "https://cdn.bin.dronk.dev/",
     salt: "IZkFH6tPnaJb1R+WlFX6fn5bJoSe4VrN3hLDk9B7HIdtkgz9sweu2nggyICFsiqTGNi1NrscQRBd"
 }
